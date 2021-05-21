2afac7e9d997887c640ed2cf1c3da56a2e45ef09
janaite <janaite@gmail.com>
2021-05-21 19:22:33 -0300

    create a new Client with POST

--- src/main/java/net/janaite/devsuperior/bootcamp/assignment1/resources/ClientResource.java
+++ src/main/java/net/janaite/devsuperior/bootcamp/assignment1/resources/ClientResource.java
@@ -1,5 +1,7 @@
 package net.janaite.devsuperior.bootcamp.assignment1.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -8,9 +10,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import net.janaite.devsuperior.bootcamp.assignment1.dto.ClientDTO;
 import net.janaite.devsuperior.bootcamp.assignment1.services.ClientService;
@@ -41,6 +46,13 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
